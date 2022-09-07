3f208c176d0b5123f23296615f59fe6b01acf5fb
iachiru <bistancu@gmail.com>
2022-09-07 18:29:13 +0100

    feat: Add extra cards and css for them. Add hamburger menu (not functional) Add css for the menu and nav bar.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //Start with empty ul and build navigation dynamically by ussing append, appendChild and innerHTML.
+
+// Create an array that can hold your 4 sections.
+
+const mainDivisions = ["seinen", "shonen", "shojo", "sports"]; // I have to find out how this can be actually the sections.
+
+// hamburger menu functionality
+
+function onClickMenu() {
+  let hamburgerMenu = document.getElementsById("h__menu");
+  hamburgerMenu.classList.toggle("change");
+}
